1c60feccbdbc8475d2b530d5ea3552fb7a7c2f23
Grace Zawadi <grayvianne@gmail.com>
2018-06-27 10:24:36 +0300

    TDD

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,16 @@
+"""
+Application package constructor
+"""
 from flask import Flask
 
+from config import CONFIG
 
-app = Flask(__name__)
+def create_app(config_name):
+    """
+    Application Factory
+    """
+    app = Flask(__name__)
+    app.config.from_object(CONFIG[config_name])
+
+
+    return app
