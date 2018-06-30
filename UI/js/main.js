d3c244c4714d5e50f11804c2217ff303c507c2bf
Grace Zawadi <grayvianne@gmail.com>
2018-06-30 20:49:40 +0300

    [feature #158413376]update UI

---
+++ UI/js/main.js
@@ -0,0 +1,17 @@
+// function myFunction() {
+//     var x = document.getElementById("topnav");
+//     if (x.className === "nav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "nav";
+//     }
+// }
+
+// function confirmDeletion(){
+// window.confirm("Are you sure you want to delete this item?");
+
+// }
+
+// function sendOffer(){
+// alert("Offer successfully set and notification sent!");
+// }
