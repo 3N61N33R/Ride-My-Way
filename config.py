b9f9ee2d1499d1ab72a2497af7a868eceaaf9b0b
Grace Zawadi <grayvianne@gmail.com>
2018-07-01 14:37:44 +0300

    configuration settings

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 class Config:
     """
     Common configurations
-    """
+    """ 
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '\x1b\xa5*G\xca\x88@\xe37\x8e\x8cP\x18\xef\xa3\xc0r\xaa\xf4\x94H3\xc3\xfd'
+    SECRET_KEY = os.getenv('SECRET_KEY') 
 
 class DevelopmentConfig(Config):
     """
