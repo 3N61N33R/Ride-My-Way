c827f851bec2802b42bc14d740df9b717918074a
Grace Zawadi <grayvianne@gmail.com>
2018-07-03 09:31:58 +0300

    [ft]updates for db

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 """App entry point"""
+from app.v2.models import User, Ride, Request
 from app import app
 
+@app.cli.command()
+def create():
+    ''' Create all db tables '''
+    User().create()
+    Ride().create()
+    Request().create()
 
-if __name__ == '__main__':
-    app.run(debug=True)
