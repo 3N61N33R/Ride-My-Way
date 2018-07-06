1f1a68ac3cf7cf5501d2607591b3590c81891874
Grace Zawadi <grayvianne@gmail.com>
2018-07-06 06:28:22 +0300

    [ft #158861059]base test

--- tests/v2_tests/base_test.py
+++ tests/v2_tests/base_test.py
@@ -1,42 +1,33 @@
-# import psycopg2
-# from flask import current_app
-# from flask_testing import TestCase
-# from app import app
-# from app.v2.models import User, Ride
+import psycopg2
+from unittest import TestCase
+from flask import current_app
+from app import app 
+from config import CONFIG as app_config
+from app.v2.models import User, Ride
 
-# class BaseTest(TestCase):
-
-#     def create_app(self):
-#         app.test_client()
-#         app.config['testing'] = True
-        
-
-#         ctx = app.app_context()
-#         ctx.push()
-
-#         return app
-
-#     def tearDown(self):
-#         conn=psycopg2.connect(f"dbname='{current_app.config['DBNAME']}' user='{current_app.config['DBUSER']}' password='{current_app.config['DBPASSWORD']}'")
-#         cur = conn.cursor()
-#         cur.execute('''DROP TABLE users CASCADE''')
-#         cur.execute('''DROP TABLE rides CASCADE''')
-        
-        
-            
-
-#     def setUp(self):
+class BaseTest(TestCase):
+    def setUp(self):
 
         # ride = Ride()
         # ride.create ("7","Fedha","Greenfiels"," 10:00a.m")
 
         # user = User()
-        # user.create("May","May","mary5@gmail.com","yolovibes")
+        app.config.from_object(app_config['testing'])
+        self.app = app.test_client()
+        ctx = app.app_context()
+        ctx.push()
 
-        # pass
 
-
-
-
-    
+        self.user = {
+	"name": "root",
+	"username": "sammy",
+	"email": "sammy@gmail.com",
+	"password": "groot"
+        }
+        user = User()
+        user.drop('users')
+        user.create()
+        ride = Ride()
+        user.drop('rides')
+        ride.create()
         
