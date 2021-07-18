8b69992ea550143bb75efab07819f5ca3c8497cf
Grace Zawadi <g.zawadi.w@gmail.com>
2021-07-19 02:28:02 +0300

    change app name

--- app/v2/__init__.py
+++ app/v2/__init__.py
@@ -1,10 +1,10 @@
-from flask import Blueprint,Flask
+from flask import Blueprint, Flask
 from flasgger import Swagger
 
-app = Flask(__name__)
-Swagger(app)
+app2 = Flask(__name__)
+Swagger(app2)
 
 
-v2 = Blueprint('v2',__name__)
+v2 = Blueprint("v2", __name__)
 
 from . import views
